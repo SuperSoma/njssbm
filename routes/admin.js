c3ec634b879340ccd7554ab7e39d41dcb6eaa9a5
Nick <nick@pereless.com>
2018-09-25 17:01:50 -0400

    Added approving and deleting of events and social links

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var ObjectId = require('mongodb').ObjectID;
 var mongoose = require('mongoose');
 var locals = require('../models/localModel.js');
 var events = require('../models/eventModel.js');
@@ -63,7 +64,48 @@ router.post('/', function(req, res, next) {
 		renderAdminPage(req, res, next);
 	} else if (req.body.adminPW == "njmelee" && typeof(req.body.updateStuff) != "undefined") {
 		console.log(req.body);
+		
+		for (var key in req.body.local) {
+			console.log(req.body.local[key]);
+		
+			switch(req.body.local[key]) {
+				case 'approve':
+				locals.update({"_id" : ObjectId(key)}, {active: true}, {}, function(err,doc) {if (err) {console.log(err)}});
+				break;
+				
+				case 'delete':
+				locals.remove({"_id" : key}, function(err,doc) {if (err) {console.log(err)}});
+				break;
+			}
+		}
+		
+		for (var key in req.body.event) {
+			switch(req.body.event[key]) {
+				case 'approve':
+				events.update({"_id" : ObjectId(key)}, {active: true}, {},function(err,doc) {if (err) {console.log(err)}});
+				break;
+				
+				case 'delete':
+				events.remove({"_id" : ObjectId(key)}, function(err,doc) {if (err) {console.log(err)}});
+				break;
+			}
+		}
+		
+		for (var key in req.body.social) {
+			switch(req.body.social[key]) {
+				case 'approve':
+				social.update({"_id" : ObjectId(key)}, {active: true}, {},function(err,doc) {if (err) {console.log(err)}});
+				break;
+				
+				case 'delete':
+				social.remove({"_id" : ObjectId(key)}, function(err,doc) {if (err) {console.log(err)}});
+				break;
+			}
+		}
+		
 		renderAdminPage(req, res, next);
+	} else {
+		res.render('adminLogin');
 	}
 });
 
@@ -72,6 +114,8 @@ router.put('/', function(req, res, next) {
 			console.log(req.body);
 			
 			renderAdminPage(req, res, next);
+		} else {
+			res.render('adminLogin');			
 		}
 });
 
